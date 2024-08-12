6332525156aa163caa78dac4b6866f58caf25e87
dredevs <contact.dredev@gmail.com>
2024-08-11 18:07:10 -0700

    yes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import '../styles/global.css'; 
+import Head from 'next/head';
+import '../styles/global.css'; // Import global styles
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
